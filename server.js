09e154fdcde4a0e5d760c69937f638672cb3546a
Marina-Romany <96652516+Marina-Romany@users.noreply.github.com>
2022-02-14 16:20:30 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const articlesRouter = require('./routes')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/',(req,res)=>{
+   res.render('index')
+})
+
+
+app.listen(5000)
